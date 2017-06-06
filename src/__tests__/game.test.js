bcda98e410f701d6cb083e7366a95d0bb23c6de7
Josef Krajkář <josef.krajkar@mensa.cz>
2017-06-06 10:38:38 +0200

    Add files via upload

--- src/__tests__/game.test.js
+++ src/__tests__/game.test.js
@@ -9,54 +9,18 @@ describe('<Game/> - rendering of components', () => {
   });
 });
 
-/*
-describe('<Resizer/> - rendering', () => {
-it('renders without crashing', () => {
-  const component = shallow(<Resizer/>);
-  expect(component).toHaveLength(1);
+describe('<Game/> - events', () => {
+it('handles the "mouseClick" event properly', () => {
+  const component = mount(<Game/>);
+  component.simulate('click');
+  expect(component.state().test_fired).toBe(true);
 });
-it('renders props correctly', () => {
-  const component = shallow(<Resizer name="resizer"/>);
-  expect(component.instance().props.name).toBe('resizer');
-});
-it('renders children when passed in', () => {
-  const component = shallow(<Resizer><div className="unique" /></Resizer> );
-  expect(component.contains(<div className="unique" />)).toBe(true);
+it('handles the "mouseMove" event properly', () => {
+  const component = mount(<Game/>);
+  component.simulate('mousemove');
+  expect(component.state().test_moved).toBe(true);
 });
 });
-
-describe('<Resizer/> - events', () => {
-it('handles the "mouseDown" event properly', () => {
-  const component = mount(<Resizer/>);
-  const resizer = component.find('div');
-  resizer.simulate('mousedown');
-  expect(component.state().clicked).toBe(true);
-});
-it('handles the "MouseUp" event properly', () => {
-  const component = mount(<Resizer/>);
-  const resizer = component.find('div');
-  resizer.simulate('mouseup');
-  expect(component.state().clicked).toBe(false);
-});
-it('handles the "MouseOut" event properly', () => {
-  const component = mount(<Resizer/>);
-  const resizer = component.find('div');
-  resizer.simulate('mouseout');
-  expect(component.state().clicked).toBe(false);
-});
-it('handles the "MouseMove" event properly', () => {
-  const component = mount(<Resizer type="both">Content</Resizer>);
-  const resizer = component.find('div');
-  let x = component.state().style.width;
-  let y = component.state().style.height;
-  resizer.simulate('mousedown');
-  resizer.simulate('mousemove');
-  expect(component.state().style.width).not.toBe(x);
-  expect(component.state().style.width).not.toBe(y);
-});
-});
-
-*/
 
 
 
